48bc97ada2cadec0d4ba3c837caeb92c017d4acb
olegweremey <olegweremey1994@mail.ru>
2022-12-18 22:52:15 +0300

    gh

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
-const {defineConfig} = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
-
 module.exports = {
-  publicPath: '/vue-app/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-app/'
+    : '/'
 }
